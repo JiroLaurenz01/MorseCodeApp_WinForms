2240eb1849ba2e2fabea9a6a32fd66d690223229
Jiro Laurenz <jirolaurenz28@gmail.com>
2023-09-13 17:59:55 +0800

    DESIGN AND FUNCTIONALITY FOR LOADING SCREEN FORM

---
+++ MORSE CODE APP/LoadingScreenForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MORSE_CODE_APP
+{
+    public partial class LoadingScreenForm : Form
+    {
+        public LoadingScreenForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingScreenForm_Load(object sender, EventArgs e)
+        {
+            // Start the timer when the form loads
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Close the form when the timer ticks (after 10 seconds)
+            Close();
+        }
+    }
+}
